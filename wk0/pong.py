9771743dde8063798540f2527f0c9ccb19a894eb
jacknoutch <jack.noutch@gmail.com>
2026-03-05 18:33:46 +0000

    The Paddle Update

--- wk0/pong.py
+++ wk0/pong.py
@@ -31,11 +31,25 @@ running = True
 
 FONT = pg.font.Font("font.ttf", 36)
 SMALL_FONT = pg.font.Font("font.ttf", 8)
+SCORE_FONT = pg.font.Font("font.ttf", 32)
+
+## Game variables
+
+player1_score = 0
+player2_score = 0
+
+player1_y = 30
+player2_y = VIRTUAL_HEIGHT - 50
+
+PADDLE_SPEED = 200
 
 
 # Main game loop -----------------------------------------
 
 while running:
+
+    # Cap the frame rate
+    dt = clock.tick(FPS) / 1000
 
     # Handle events
     for event in pg.event.get():
@@ -46,23 +60,41 @@ while running:
             if event.key == pg.K_ESCAPE:
                 running = False
 
+    keys = pg.key.get_pressed()
+    
+    if keys[pg.K_s]:
+        player1_y += PADDLE_SPEED * dt
+    if keys[pg.K_w]:
+        player1_y -= PADDLE_SPEED * dt
+    if keys[pg.K_DOWN]:
+        player2_y += PADDLE_SPEED * dt
+    if keys[pg.K_UP]:
+        player2_y -= PADDLE_SPEED * dt
+
     # Rendering
     game_surface.fill(BACKGROUND_COLOUR)
 
-    pg.draw.rect(game_surface, WHITE, (10, 30, 5, 20))
-    pg.draw.rect(game_surface, WHITE, (VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20))
+    ## Paddles
+    pg.draw.rect(game_surface, WHITE, (10, player1_y, 5, 20))
+    pg.draw.rect(game_surface, WHITE, (VIRTUAL_WIDTH - 10, player2_y, 5, 20))
+    
+    ## Ball
     pg.draw.rect(game_surface, WHITE, (VIRTUAL_WIDTH // 2 - 2, VIRTUAL_HEIGHT // 2 - 2, 4, 4))
 
+    ## Title
     text = SMALL_FONT.render("Hello Pong!", False, WHITE)
     game_surface.blit(text, (VIRTUAL_WIDTH // 2 - text.get_width() // 2, 0))
+
+    ## Score
+    score1 = SCORE_FONT.render(str(player1_score), False, WHITE)
+    score2 = SCORE_FONT.render(str(player2_score), False, WHITE)
+    game_surface.blit(score1, (VIRTUAL_WIDTH // 2 - 50, VIRTUAL_HEIGHT // 3))
+    game_surface.blit(score2, (VIRTUAL_WIDTH // 2 + 30, VIRTUAL_HEIGHT // 3))
 
     # Scale the game surface to fit the window
     scaled = pg.transform.scale(game_surface, screen.get_size())
     screen.blit(scaled, (0, 0))
 
     pg.display.flip()
-
-    # Cap the frame rate
-    clock.tick(FPS)
 
 pg.quit()
