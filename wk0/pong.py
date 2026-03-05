e11a00e28c7b5ec406b964df44840b04802789ab
jacknoutch <jack.noutch@gmail.com>
2026-03-05 17:39:09 +0000

    The Day-0 Update

--- wk0/pong.py
+++ wk0/pong.py
@@ -1,10 +1,18 @@
 import pygame as pg
 
+# Settings
+FPS = 60
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+WHITE = (255, 255, 255)
+
+
 # Initialize Pygame
 pg.init()
-screen = pg.display.set_mode((1280, 720))
+screen = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pg.time.Clock()
 running = True
+
 
 # Main game loop
 while running:
@@ -16,9 +24,14 @@ while running:
 
     # Rendering
     screen.fill((0, 0, 0))
+
+    font = pg.font.Font(None, 36)
+    text = font.render("Hello Pong!", True, WHITE)
+    screen.blit(text, (WINDOW_WIDTH // 2 - text.get_width() // 2, WINDOW_HEIGHT // 2 - text.get_height() // 2))
+
     pg.display.flip()
 
     # Cap the frame rate
-    clock.tick(60)
+    clock.tick(FPS)
 
 pg.quit()
